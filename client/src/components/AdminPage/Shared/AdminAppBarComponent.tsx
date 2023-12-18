78b35581723aa777cc7acaa4f2c4d2773413e941
Alan Bardales <alan.g.bardales@gmail.com>
2023-12-18 14:28:25 -0600

    Added back button to AdminAppBarComponent

--- client/src/components/AdminPage/Shared/AdminAppBarComponent.tsx
+++ client/src/components/AdminPage/Shared/AdminAppBarComponent.tsx
@@ -1,14 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 interface AdminAppBarComponentProps {
     title: string
+    backUrl?: string | null
 }
-export default function AdminAppBarComponent({ title }: AdminAppBarComponentProps){
+export default function AdminAppBarComponent({ title, backUrl }: AdminAppBarComponentProps){
+    const navigate = useNavigate();
+
     return (
         <AppBar position='static' style={{background: "#2E3B55", marginBottom: '1rem'}}>
             <Toolbar variant='dense'>
+                {backUrl ? (
+                    <IconButton sx={{color: 'white'}} onClick={() => navigate(backUrl)}>
+                        <ArrowBackIcon />
+                    </IconButton>
+                ) : null}
                 <Typography variant="h4" style={{marginBottom: '1rem', marginTop: '1rem'}}>{title}</Typography>
             </Toolbar>
         </AppBar>
